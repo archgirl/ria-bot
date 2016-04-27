11ed8bb79df30499bcea5d93d8287990ef1babdb
Melanie E Magdalena <info@melaniemagdalena.com>
2016-04-26 22:00:09 -0600

    template file added
    does not include destination channel information

---
+++ template.js
@@ -0,0 +1,30 @@
+var tmi = require('tmi.js');
+
+var options = {
+  options: {
+    debug: true
+  },
+  connection: {
+    cluster: "aws",
+    reconnect: true
+  },
+  identity: {
+    username: "RIA_bot",
+    password: "oauth:x9ym6x73yq7j044848w0h32yjnqjys"
+  },
+  channels: ["channel"]
+};
+
+var client = new tmi.client(options);
+client.connect();
+
+client.on('chat', function(channel, user, message, self) {
+  if(message === "!ria") {
+    client.action("channel", "RIA is a remote intelligence agent, a chat bot for all your streaming needs.");
+  }
+});
+
+client.on('connected', function(address, port) {
+  console.log("Address: " + address + "Port: " + port);
+  client.action("channel", "RIA is now connected to Channel Name.")
+});
